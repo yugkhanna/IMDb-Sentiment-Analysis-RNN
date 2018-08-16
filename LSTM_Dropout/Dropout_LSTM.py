d2c9cc4e4340ff312cdb0bfba800ef183cf6c79f
Yug Khanna <yugkhanna1@gmail.com>
2018-08-16 15:16:10 +0530

    Model added

---
+++ LSTM_Dropout/Dropout_LSTM.py
@@ -0,0 +1,35 @@
+from keras.datasets import imdb
+
+vocabulary_size = 7000
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words = vocabulary_size)
+
+from keras.preprocessing import sequence
+
+max_words = 400
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+
+from keras import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout, MaxPool1D, Conv1D, Conv2D
+
+import numpy as np
+seed = 8
+np.random.seed(8)
+
+model = Sequential()
+embedding_size=64
+model=Sequential()
+model.add(Embedding(vocabulary_size, embedding_size, input_length=max_words))
+model.add(Dropout(0.25))
+model.add(LSTM(200))
+model.add(Dropout(0.25))
+model.add(Dense(1, activation='sigmoid'))
+
+print(model.summary())
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=128, verbose=1)
+
+evaluation = model.evaluate(X_test, y_test)
+print("Accuracy %f%%" % (evaluation[1] * 100))
