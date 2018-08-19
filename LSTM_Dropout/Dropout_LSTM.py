aa27de2d88a076a0562da5707cd0ab2e5e50e464
Yug Khanna <yugkhanna1@gmail.com>
2018-08-19 16:08:02 +0530

    Update Dropout_LSTM.py

--- LSTM_Dropout/Dropout_LSTM.py
+++ LSTM_Dropout/Dropout_LSTM.py
@@ -1,6 +1,10 @@
 from keras.datasets import imdb
 
 vocabulary_size = 7000
+"""
+Setting vocabulary_size to 7000 trims the dataset length to get
+only 7000 words from the dataset to load into our variables.
+"""
 
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words = vocabulary_size)
 
@@ -21,7 +25,7 @@ from keras import Sequential
 from keras.layers import Embedding, LSTM, Dense, Dropout, MaxPool1D, Conv1D, Conv2D
 
 import numpy as np
-seed = 8
+seed = 8 # Seed is set to 8 to provide consistency in training 
 np.random.seed(8)
 
 model = Sequential()
